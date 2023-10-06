ba71befada8744a8358b62c73a4ecab567724578
Josh Elliott <josh@joshelliott.me>
2023-10-06 13:46:20 -0400

    Fixing the config.js to be in the correct file

--- vitest.config.js
+++ vitest.config.js
@@ -5,7 +5,6 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    base: '/LifeCounter/',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
